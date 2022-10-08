c20ba2f70586a6c6521d5dc74af238c6542b10bb
Sayan Roy <rsayan553@gmail.com>
2022-10-09 00:14:52 +0530

    OpenCV Guide - Different Image Processing techniques are added

---
+++ OpenCV Guide/utilities.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import cv2
+
+def show_img(img, figsize: tuple = (15, 15)):
+    fig = plt.figure(figsize = figsize)
+    ax = fig.add_subplot(111)
+    ax.imshow(img, cmap="gray")
+    plt.show()
+    
+def load_img(img_path: str):
+    img = cv2.imread(img_path).astype(np.float32) / 255
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
